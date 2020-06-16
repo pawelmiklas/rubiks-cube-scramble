521cd10ace25c181f0c9ce0122aaf97142a01531
Paweł Mikłas <pablomiklas@gmail.com>
2020-06-17 00:04:15 +0200

    added scoreboard for 2 and 3

--- rubiks-cube-scramble/Program.cs
+++ rubiks-cube-scramble/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace rubiks_cube_scramble
 {
@@ -6,7 +8,63 @@ namespace rubiks_cube_scramble
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Scoreboard> scoreboard3 = new List<Scoreboard>();
+            List<Scoreboard> scoreboard2 = new List<Scoreboard>();
+            
+            scoreboard3.Add(new Scoreboard { Scramble = "U2 L B2 R U R2 U' F2 R2 B R2 L2 D", Time = "51.33"});
+            scoreboard3.Add(new Scoreboard { Scramble = "B L R2 D2 U2 R' U D B L B D' L U'", Time = "11.12"});
+            scoreboard3.Add(new Scoreboard { Scramble = "D' B' R' B' R' U D2 L2 U R2 U' F2", Time = "31.33"});
+            
+            scoreboard2.Add(new Scoreboard { Scramble = "U2 L B2 R2 U' F2 R2 B", Time = "7.64"});
+            scoreboard2.Add(new Scoreboard { Scramble = "B L R2 D2 U2 B D' L U'", Time = "8.41"});
+            scoreboard2.Add(new Scoreboard { Scramble = "D' B' R' B' R R2 U' F2", Time = "9.53"});
+            
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("===========================================");
+                Console.WriteLine("1. Scoreboard for 3x3x3");
+                Console.WriteLine("2. Scoreboard for 2x2x2");
+                Console.WriteLine("3. Timer with scramble for 3x3x3");
+                Console.WriteLine("4. Timer with scramble for 3x3x3");
+                Console.WriteLine("===========================================");
+                Console.WriteLine("Pick your option..");
+                
+                ConsoleKey key;
+                key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.D1)
+                {
+                    bool isNotCorrectKey = true;
+                    while (isNotCorrectKey)
+                    {
+                        Console.Clear();
+                        List<Scoreboard> sortedScoreboard3 = scoreboard3.OrderBy(o => o.Time).ToList();
+
+                        for (int i = 0; i < sortedScoreboard3.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. Time: {sortedScoreboard3[i].Time}, Scramble: {sortedScoreboard3[i].Scramble}");
+                        }
+                        Console.WriteLine("Type 'e' if you want back to menu");
+                        key = Console.ReadKey(true).Key;
+                        if (key == ConsoleKey.E)
+                        {
+                            isNotCorrectKey = false;
+                        }
+                    }
+                } else if (key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("2");
+                } else if (key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("3");
+                } else if (key == ConsoleKey.D4)
+                {
+                    Console.WriteLine("4");
+                }
+
+                // Console.ReadKey();
+            }
         }
     }
 }
---
+++ rubiks-cube-scramble/scoreboard.cs
@@ -0,0 +1,8 @@
+namespace rubiks_cube_scramble
+{
+    public class Scoreboard
+    {
+        public string Time { get; set; }
+        public string Scramble { get; set; }
+    }
+}
